340573345380e8e452c38372f40543e271a2f373
Moka <144582917+Mo-Kasaei@users.noreply.github.com>
2024-10-31 10:36:15 +0330

    Update commitmentGenerator.cpp

--- gcc/V2.0/commitmentGenerator.cpp
+++ gcc/V2.0/commitmentGenerator.cpp
@@ -556,7 +556,7 @@ void fidesinnova::commitmentGenerator(String path, int64_t g, int64_t p) {
 /*
 Read program.s and insert assembly of store register based on the line provided by device_config.json
 
-Add proofGenerator function in assembly at the end of the lines.
+Add proofGenerator function in assembly at the end of the lines and store it in program_new.s.
 */
 
 
