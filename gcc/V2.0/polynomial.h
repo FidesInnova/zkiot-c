f42a3b6874660ef17176022e3488edc5c520c0aa
Moka <144582917+Mo-Kasaei@users.noreply.github.com>
2024-10-31 09:44:18 +0330

    Update polynomial.h

--- gcc/V2.0/polynomial.h
+++ gcc/V2.0/polynomial.h
@@ -10,56 +10,56 @@ using namespace std;
 
 class Polynomial {
 public:
-  // Function to compute the modular exponentiation (base^exponent) % mod
-  static int64_t power(int64_t base, int64_t exponent, int64_t mod);
+  // Function to compute the p exponentiation (base^exponent) % p
+  static int64_t power(int64_t base, int64_t exponent, int64_t p);
 
-  // Function to compute the modular exponentiation (a^b) % mod
-  static int64_t modExp(int64_t a, int64_t b, int64_t mod);
+  // Function to compute the p exponentiation (a^b) % p
+  static int64_t pExp(int64_t a, int64_t b, int64_t p);
 
-  // Function to compute the modular inverse using Fermat's Little Theorem
-  static int64_t modInverse(int64_t a, int64_t mod);
+  // Function to compute the p inverse using Fermat's Little Theorem
+  static int64_t pInverse(int64_t a, int64_t p);
 
-  // Function to generate a random number in mod
-  static int64_t generateRandomNumber(const vector<int64_t>& H, int64_t mod);
+  // Function to generate a random number in p
+  static int64_t generateRandomNumber(const vector<int64_t>& H, int64_t p);
 
-  // Add two polynomials with modular arithmetic
-  static vector<int64_t> addPolynomials(const vector<int64_t>& poly1, const vector<int64_t>& poly2, int64_t mod);
+  // Add two polynomials with p arithmetic
+  static vector<int64_t> addPolynomials(const vector<int64_t>& poly1, const vector<int64_t>& poly2, int64_t p);
 
-  // Subtract two polynomials with modular arithmetic
-  static vector<int64_t> subtractPolynomials(const vector<int64_t>& poly1, const vector<int64_t>& poly2, int64_t mod);
+  // Subtract two polynomials with p arithmetic
+  static vector<int64_t> subtractPolynomials(const vector<int64_t>& poly1, const vector<int64_t>& poly2, int64_t p);
 
   // Function to multiply two polynomials
-  static vector<int64_t> multiplyPolynomials(const vector<int64_t>& poly1, const vector<int64_t>& poly2, int64_t mod);
+  static vector<int64_t> multiplyPolynomials(const vector<int64_t>& poly1, const vector<int64_t>& poly2, int64_t p);
 
   // Function to divide two polynomials
-  static vector<vector<int64_t>> dividePolynomials(const vector<int64_t>& dividend, const vector<int64_t>& divisor, int64_t mod);
+  static vector<vector<int64_t>> dividePolynomials(const vector<int64_t>& dividend, const vector<int64_t>& divisor, int64_t p);
 
   // Function to multiply a polynomial by a number
-  static vector<int64_t> multiplyPolynomialByNumber(const vector<int64_t>& H, int64_t h, int64_t mod);
+  static vector<int64_t> multiplyPolynomialByNumber(const vector<int64_t>& H, int64_t h, int64_t p);
 
   // Function to compute Lagrange basis polynomial L_i(x)
-  static vector<int64_t> LagrangePolynomial(int64_t i, const vector<int64_t>& x_values, int64_t mod);
+  static vector<int64_t> LagrangePolynomial(int64_t i, const vector<int64_t>& x_values, int64_t p);
 
   // Function to compute Lagrange polynomial(x, y)
-  static vector<int64_t> setupLagrangePolynomial(const vector<int64_t> x_values, const vector<int64_t> y_values, int64_t mod, const String& name);
+  static vector<int64_t> setupLagrangePolynomial(const vector<int64_t> x_values, const vector<int64_t> y_values, int64_t p, const String& name);
 
   // Function to parse the polynomial string and evaluate it
-  static int64_t evaluatePolynomial(const vector<int64_t>& polynomial, int64_t x, int64_t mod);
+  static int64_t evaluatePolynomial(const vector<int64_t>& polynomial, int64_t x, int64_t p);
 
   // Function to compute the sum of polynomial evaluations at multiple points
-  static int64_t sumOfEvaluations(const vector<int64_t>& poly, const vector<int64_t>& points, int64_t mod);
+  static int64_t sumOfEvaluations(const vector<int64_t>& poly, const vector<int64_t>& points, int64_t p);
 
   // Function to create a polynomial for (x - root)
   static vector<int64_t> createLinearPolynomial(int64_t root);
 
   // Function to calculate Polynomial r(α,x) = (alpha^n - x^n) / (alpha - x)
-  static vector<int64_t> calculatePolynomial_r_alpha_x(int64_t alpha, int64_t n, int64_t mod);
+  static vector<int64_t> calculatePolynomial_r_alpha_x(int64_t alpha, int64_t n, int64_t p);
 
   // Function to calculate Polynomial r(α,x) = (alpha^n - x^n) / (alpha - x)
-  static int64_t calculatePolynomial_r_alpha_k(int64_t alpha, int64_t k, int64_t n, int64_t mod);
+  static int64_t calculatePolynomial_r_alpha_k(int64_t alpha, int64_t k, int64_t n, int64_t p);
 
   // Function to expand polynomials given the roots
-  static vector<int64_t> expandPolynomials(const vector<int64_t>& roots, int64_t mod);
+  static vector<int64_t> expandPolynomials(const vector<int64_t>& roots, int64_t p);
 
   // Function to print polynomial in serial
   static void printPolynomial(const vector<int64_t>& coefficients, const String& name);
@@ -86,16 +86,16 @@ public:
   static void printMapping(vector<vector<int64_t>>& row, const String& name);
 
   // Function to create the val mapping
-  static vector<vector<int64_t>> valMapping(const vector<int64_t>& K, const vector<int64_t>& H, vector<vector<int64_t>>& nonZeroRows, vector<vector<int64_t>>& nonZeroCols, int64_t mod);
+  static vector<vector<int64_t>> valMapping(const vector<int64_t>& K, const vector<int64_t>& H, vector<vector<int64_t>>& nonZeroRows, vector<vector<int64_t>>& nonZeroCols, int64_t p);
 
-  // Function to calculate log in mod
-  static int64_t log_mod(int64_t a, int64_t b, int64_t mod);
+  // Function to calculate log in p
+  static int64_t log_p(int64_t a, int64_t b, int64_t p);
 
-  // Function to calculate e_func in mod
-  static int64_t e_func(int64_t a, int64_t b, int64_t g, int64_t mod);
+  // Function to calculate e_func in p
+  static int64_t e_func(int64_t a, int64_t b, int64_t g, int64_t p);
 
-  // Function to calculate KZG in mod
-  static int64_t KZG_Commitment(vector<int64_t> a, vector<int64_t> b, int64_t mod);
+  // Function to calculate KZG in p
+  static int64_t KZG_Commitment(vector<int64_t> a, vector<int64_t> b, int64_t p);
 };
 
 #endif  // POLYNOMIAL_H
