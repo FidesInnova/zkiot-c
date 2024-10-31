1ec425e18c89627b4ca5b4985f9f6f7a0fa0039d
Moka <144582917+Mo-Kasaei@users.noreply.github.com>
2024-10-31 09:37:36 +0330

    Update main.cpp

--- gcc/V2.0/main.cpp
+++ gcc/V2.0/main.cpp
@@ -16,16 +16,16 @@ void setup() {
     delay(10);
   }
 
-  uint64_t mod = 181;  // Initialize the static member 18446744069414584321 | 2013265921 | 181
+  uint64_t p = 181;  // Initialize the static member 18446744069414584321 | 2013265921 | 181
   uint64_t g = 2;      // Initialize g 33 | 2
   uint64_t n = 5;      // Initialize m
   uint64_t b = 2;      // Initialize b
   uint64_t tau = 119;  // Initialize tau
 
-  lib.setup(g, tau, mod);
-  lib.commitmentGenerator("instruction.txt", g, mod);
-  lib.proofGenerator("instruction.txt", g, b, mod);
-  lib.verifier(g, mod);
+  lib.setup(g, tau, p);
+  lib.commitmentGenerator("instruction.txt", g, p);
+  lib.proofGenerator("instruction.txt", g, b, p);
+  lib.verifier(g, p);
 
 
   // Read the registers
